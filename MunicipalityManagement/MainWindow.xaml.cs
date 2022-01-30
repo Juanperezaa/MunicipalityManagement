eaa0028331aae93eab4e71dc887ad56efcc45827
JUAN SEBASTIÁN PEREZ CAMACHO <1006286728@u.icesi.edu.co>
2022-01-29 21:33:02 -0500

    Table working

--- MunicipalityManagement/MainWindow.xaml.cs
+++ MunicipalityManagement/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
 using System.IO;
+using System.Linq;
 using System.Windows;
 using Microsoft.Win32;
 
@@ -25,30 +26,29 @@ namespace MunicipalityManagement
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        { 
-            if (ofd.ShowDialog()!=null)
+       /* private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (ofd.ShowDialog() != null)
             {
                 //Get the path of specified file
                 filePath = ofd.FileName;
                 //txtEditor.Text=filePath;
                 //MessageBox.Show(filePath);
-                ReadCsv();
+                //ReadCsv();
             }
-            
-        }
+           // dataGridTable.DataContext = data;
+        }*/
 
-        private void ReadCsv()
+        private IEnumerable<Department> ReadCsv(String filePath)
         {
-            var Reader = new StreamReader(File.OpenRead(filePath));
-            
-            Header = Reader.ReadLine().Split(separator);
-            while (Reader.ReadLine != null)
+
+            String[] reading = File.ReadAllLines(filePath);
+            return reading.Select(line =>
             {
-                String temp = Reader.ReadLine();
-                finalInfo.Add(temp);
-            }
-            CreateTable();
+                string[] data = line.Split(",");
+                return new Department(data[0], data[1], (data[2]), data[3], data[4]);
+            });
+           // CreateTable();
         }
 
         private void CreateTable() {
@@ -56,13 +56,42 @@ namespace MunicipalityManagement
             {
                 data.Columns.Add(Header[i], typeof(String));
             }
-            for(int i = 0; i < finalInfo.Count; i++)
+            for (int i = 0; i < finalInfo.Count; i++)
             {
                 string[] temp = finalInfo[i].Split(separator);
-                data.Rows.Add(temp[0],temp[1],temp[2],temp[3],temp[4]);
+                data.Rows.Add(temp[0], temp[1], temp[2], temp[3], temp[4]);
             }
         }
 
 
+        private void LoaderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ofd.ShowDialog() != null)
+            {
+                filePath = ofd.FileName;
+            }
+            ListView.ItemsSource = ReadCsv(filePath);
+        }
     }
+
+       
 }
+public partial class Department{
+    public String departmentCode { get; set; }
+    public String municipalityCode { get; set; }
+    public String departmentName { get; set; }
+    public String MunicipalityName { get; set; }
+   public  String type { get; set; }
+    public Department(String departmentCode,String municipalityCode,
+        String departmentName,String municipalityName, String type) {
+        this.departmentCode = departmentCode;
+        this.type = type;
+        this.departmentName = departmentName;
+        this.MunicipalityName = municipalityName;
+        this.municipalityCode = municipalityCode;
+    }
+
+    
+}
+
+
